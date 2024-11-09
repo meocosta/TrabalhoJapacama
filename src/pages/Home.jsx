3f1b4b9e4fcfaee0b7293509d1934ce541459f2e
meocosta <me25032008@gmail.com>
2024-11-09 11:08:55 -0300

    mudando home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,13 +10,10 @@ function Home() {
     const [ultimasFotos1, setUltimasFotos1] = useState([]);
     const [ultimasFotos2, setUltimasFotos2] = useState([]);
     const [Booleano, setBooleano] = useState(true);
-
-    const [ultimosArquivos, setUltimosArquivos] = useState([]);
-    const [view, setView] = useState([]);
-    const [array1, setArray1] = useState([]);
-    const [array2, setArray2] = useState([]);
-    const [carrosel, setCarrosel] = useState(true);
     const [carroselImg, setCarroselImg] = useState(true);
+
+    const[conteudoGrande, setconteudoGrande] = useState(false);
+
 
     //verifica se é um array e armazena 5 primeiros no primeiro e entre 5 e 10 no segundo
     useEffect(() => {
@@ -29,7 +26,7 @@ function Home() {
                 setUltimasFotos1(fotos);
                 setCarroselImg(false);
             }
-        } 
+        }
     }, [fotos]);
 
     //alterna entre um e outro a cada 5 segundos
@@ -46,63 +43,10 @@ function Home() {
             setFotosParaExibir(ultimasFotos1);
             settotalFotos(ultimasFotos1.length)
         }
-        
+
     }, [Booleano, ultimasFotos1.length, ultimasFotos2.length]);
 
-
-    //código artigos
-    useEffect(() => {
-        if (arquivos.length >= 10) {
-            setUltimosArquivos(arquivos.slice(-10));
-        } else {
-            setUltimosArquivos(arquivos);
-            setCarrosel(false)
-        }
-    }, [arquivos]);
-
-
-
-    useEffect(() => {
-
-        const ChangeArray1 = [];
-        const ChangeArray2 = [];
-        const ArqSelect = ultimosArquivos;
-
-        for (let count = 1; count <= ArqSelect.length; count++) {
-            if (count <= 5) {
-                ChangeArray1.push(ArqSelect[ArqSelect.length - count])
-            } else if (count <= 10) {
-                ChangeArray2.push(ArqSelect[ArqSelect.length - count])
-            }
-        }
-
-        setArray1(ChangeArray1)
-        setArray2(ChangeArray2)
-        setView(ChangeArray1);
-
-    }, [ultimosArquivos])
-
-
-    useEffect(() => {
-        if (carrosel) {
-            let showArray1 = true;
-
-            const interval = setInterval(() => {
-                if (showArray1) {
-                    setView(array1);
-                } else {
-                    setView(array2);
-                }
-                showArray1 = !showArray1;
-            }, 5000);
-
-            return () => clearInterval(interval);
-        }
-
-    }, [array1, array2]);
-
-
-
+    
     return (
         <>
 
@@ -140,21 +84,17 @@ function Home() {
                 <hr />
             </section>
 
-            <section id='CarroselArtigos-container'>
-                <h3>Veja os <Link to='/conteudo'>artigos</Link> produzidos por eles!</h3>
-                <div id='carrosel'>
-                    {arquivos.length === 0 ? <p>Nenhum artigo por enquanto, volte mais tarde!</p> : (
-                        view.map((arquivo, index) => (
-                            <div className='artigoCarrosel' id='example' key={index}>
-                                <h3>{arquivo.nome}</h3>
-                                <div className='ImgArtigoCarrosel'>
-                                    <img src={arquivo.imagem} alt={"foto do " + arquivo.nome}></img>
-                                </div>
-                                <p>{arquivo.descrição}</p>
-                                <Link to={arquivo.href}><button>ler</button></Link>
-                            </div>
-                        ))
-                    )}
+            <section id='Conteudo'>
+                <h3>Veja o <Link to='/conteudo'>conteudo</Link> produzidos por eles!</h3>
+                <div id='txt'>
+                    <p><Link to='/conteudo'>
+                        O modelo geocêntrico foi uma das primeiras concepções do universo e sustentava que
+                        a Terra era o centro do cosmos, com todos os corpos celestes, como o Sol, a Lua e as estrelas, girando ao
+                        seu redor. Esta teoria foi amplamente aceita durante a Antiguidade e a Idade Média, dominando o pensamento
+                        científico por mais de mil anos. Este trabalho examina a origem, o desenvolvimento e o impacto histórico do
+                        modelo geocêntrico, além de destacar a sua substituição por teorias mais avançadas, como o modelo
+                        heliocêntrico...
+                    </Link></p>
                 </div>
             </section>
         </>
